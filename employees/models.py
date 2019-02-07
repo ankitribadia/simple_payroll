4f50b2e615ce1ae6ccfb918906c23fbd5a3a03d9
Ankit <ankitribadia@yahoo.co.in>
2019-02-07 22:33:07 +0530

    added basic models

---
+++ employees/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+
+# Create your models here.
+class AbstractBaseModel(models.Model):
+    """AbstractBaseModel contains common fields between models.
+    All models should extend this class.
+    """
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Department(AbstractBaseModel):
+    department = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.department
+
+
+class Employee(AbstractBaseModel):
+    full_name = models.CharField(max_length=200)
+    emp_id = models.IntegerField()
+    dob = models.DateField()
+    date_joined = models.DateField()
+    department = models.ForeignKey(Department, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return (self.full_name, self.department)
