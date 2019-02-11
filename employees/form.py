a395ca43543b3debe2ca145370d631c9fc1ec59b
AnkitR <ankitribadia@yahoo.co.in>
2019-02-11 19:01:59 +0530

    removed excess code

--- employees/form.py
+++ employees/form.py
@@ -1,8 +1,5 @@
 from django import forms
 
-from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Submit
-
 from .models import Employee
 
 # for adding datepicker
@@ -19,8 +16,8 @@ class EmployeeCreateForm(forms.ModelForm):
         }
 
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.helper = FormHelper()
-        self.helper.form_method = 'post'
-        self.helper.add_input(Submit('submit', 'Save'))
+    # def __init__(self, *args, **kwargs):
+    #     super().__init__(*args, **kwargs)
+    #     self.helper = FormHelper()
+    #     self.helper.form_method = 'post'
+    #     self.helper.add_input(Submit('submit', 'Save'))
