2eeb7cb8dbc21342327352867b3966d9da03e60b
Ankit Ribadia <ankitribadia@yahoo.co.in>
2019-03-12 18:48:53 +0530

    added new models to employee app

--- employees/admin.py
+++ employees/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from .models import Department, Employee
+from .models import Department, Employee, Shift, Designation
 
 # Register your models here.
-admin.site.register(Department)
+
 admin.site.register(Employee)
+admin.site.register(Department)
+admin.site.register(Shift)
+admin.site.register(Designation)
--- employees/models.py
+++ employees/models.py
@@ -23,6 +23,25 @@ class Department(AbstractBaseModel):
         return self.department
 
 
+class Shift(models.Model):
+    shift_name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+    time_start = models.CharField(max_length=5)
+    time_end = models.CharField(max_length=5)
+    shift_duration = models.CharField(max_length=5)
+    shift_month_duration = models.CharField(max_length=5)
+
+    def __str__(self):
+        return self.shift_name
+
+
+class Designation(AbstractBaseModel):
+    designation = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.designation
+
+
 zero = Decimal('0.00')
 
 
@@ -32,7 +51,10 @@ class Employee(AbstractBaseModel):
     dob = models.DateField(blank=True)
     date_joined = models.DateField(blank=True)
     department = models.ForeignKey(Department, on_delete=models.CASCADE)
+    designation = models.ForeignKey(Designation, on_delete=models.CASCADE)
+    shift = models.ForeignKey(Shift, on_delete=models.CASCADE)
     salary = models.DecimalField(max_digits=9, decimal_places=2, default=zero)
 
     def __str__(self):
         return (self.full_name)
+
